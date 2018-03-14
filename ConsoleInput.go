65b42b35b6ae7fe69e81295de98147e11f08c54a
rahulrajewar <rahulrajewar94217@gmail.com>
2018-03-14 11:52:39 -0700

    Add files via upload

---
+++ ConsoleInput.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	//var s string
+	//fmt.Scanln(&s)
+	//fmt.Println(s)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Text: ")
+	str, _ := reader.ReadString('\n')
+	fmt.Println(str)
+
+	fmt.Print("Enter a number: ")
+	str, _ = reader.ReadString('\n')
+	f, err := strconv.ParseFloat(strings.TrimSpace(str),64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("value of a number: ", f)
+	}
+}
